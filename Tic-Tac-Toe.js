1e93845a2712345cd5aaea51a5214ca3faeb4ed9
Jennifer Hanson <jenniferhanson.sd@gmail.com>
2023-03-08 23:35:37 -0800

    Updated code fixing bugs

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -11,11 +11,14 @@ const winningCombos = [
 	[2, 4, 6]
 ];
 
-const cellElements = document.querySelectorAll('[data-cell]');
+const cellElements = document.querySelectorAll('.cell');
+console.log(cellElements);
 const boardElement = document.getElementById('board');
+console.log(boardElement);
 const winningMessageElement = document.getElementById('winningMessage');
 const restartButton = document.getElementById('restartButton');
 const winningMessageTextElement = document.getElementById('winningMessageText');
+console.log(winningMessageTextElement);
 let isOturn = false;
 
 startGame();
@@ -30,7 +33,15 @@ function startGame() {
 		cell.removeEventListener('click', handleCellClick);
 		cell.addEventListener('click', handleCellClick, { once: true })
 	})
-	setBoardHoverClass();
+    console.log("test");
+    // for (let i = 0; i < cellElements.length; i++){
+    //     console.log(cellElements);
+    // //     cell.classList.remove(X);
+	// // 	cell.classList.remove(O);
+	// // 	cell.removeEventListener('click', handleCellClick);
+	// // 	cell.addEventListener('click', handleCellClick, { once: true })
+	// //}
+	setBoardHoverClass();  
     console.log(setBoardHoverClass);
 	winningMessageElement.classList.remove('show')
 }
@@ -38,14 +49,53 @@ function startGame() {
 function handleCellClick(e) {
 	const cell = e.target;
 	const currentTurn= isOturn ? O : X;
-	placeMark(cell, currentTurn)
+    placeMark(cell, currentTurn)
 	if (checkWin(currentTurn)) {
 		endGame(false)
+        //console.log(checkWin);
+        //console.log(endGame);
 	} else if (isDraw()) {
 		endGame(true)
 	} else {
 		swapTurns()
 		setBoardHoverClass()
+	}
+    //console.log(cell);
+    //console.log(currentTurn);
+
+}
+
+function endGame(draw) {
+    if(draw) {
+        winningMessageTextElement.innerHTML = "It is a draw!"
+    } else {
+        winningMessageTextElement.innerHTML = `Player with ${isOturn ? "O's" : "X's"} wins!`
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return[...cellElements].every(cell => {
+        return cell.classList.contains(X) || cell.classList.contains(O)
+    })
+}
+
+function placeMark(cell, currentTurn) {
+	cell.classList.add(currentTurn)
+    console.log(cell.classList)
+}
+
+function swapTurns() {
+	isOturn = !isOturn
+}
+
+function setBoardHoverClass() {
+	boardElement.classList.remove(X)
+	boardElement.classList.remove(O)
+	if (isOturn) {
+		boardElement.classList.add(O)
+	} else {
+		boardElement.classList.add(X)
 	}
 }
 
@@ -56,4 +106,3 @@ function checkWin(currentTurn) {
 		})
 	})
 }
-
